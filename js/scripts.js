84ded3d340736196947866ed3b2499130a9478f0
Steve Vickers <stevev1970@gmail.com>
2020-11-28 11:24:41 -0500

    Week 4 Workshop

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+$(function () {
+    $("#loginButton").click(function () {
+        $("#loginModal").modal('show');
+
+    });
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal('show');
+    });
+    });
